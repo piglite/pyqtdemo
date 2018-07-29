bb76151aebb51bbcdcae3e22dc24ba2c5f794dea
piglite <piglite@vip.sina.com>
2018-07-29 16:32:44 +0800

    2018/7/29 add image

--- pyqt_test/qss_selector.py
+++ pyqt_test/qss_selector.py
@@ -1,9 +1,10 @@
 import sys
 
-from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QApplication
+from PyQt5.QtGui import QWindow
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QApplication, QMainWindow
 
 
-class MyWindow(QWidget):
+class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.btn1 = QPushButton('按钮1')
@@ -13,6 +14,7 @@ class MyWindow(QWidget):
         layout.addWidget(self.btn1)
         layout.addWidget(self.btn2)
         self.setLayout(layout)
+        self.resize(640,480)
         self.setWindowTitle('QSS Demo')
 
 app = QApplication(sys.argv)
